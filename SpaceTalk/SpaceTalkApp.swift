f236ebc8a95e12f00e157a745e2f3453b62f7f0a
Jo SeongBin <avan31017@naver.com>
2023-03-25 21:21:17 +0900

    Initial Commit

---
+++ SpaceTalk/SpaceTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTalkApp.swift
+//  SpaceTalk
+//
+//  Created by 조성빈 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SpaceTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
